5fca7012cfadc5d460d595f384b0a44694c51a11
rayweb <raymundo.armendariz@gmail.com>
2019-02-05 14:32:07 -0700

    .

--- src/main/java/com/rayweb/proposedtalk/controller/ProposedTalkController.java
+++ src/main/java/com/rayweb/proposedtalk/controller/ProposedTalkController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProposedTalkController {
 
-    @SuppressWarnings("SameReturnValue")
     @GetMapping(value="/")
     public String home() {
         return "talks";

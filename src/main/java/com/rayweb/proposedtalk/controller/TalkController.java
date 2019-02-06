6d7812d0bc9e1495dc8ac5b8b677e8d437525c55
rayweb <raymundo.armendariz@gmail.com>
2019-02-05 18:27:20 -0700

    adding logic to post a talk

--- src/main/java/com/rayweb/proposedtalk/controller/TalkController.java
+++ src/main/java/com/rayweb/proposedtalk/controller/TalkController.java
@@ -1,9 +1,15 @@
 package com.rayweb.proposedtalk.controller;
 
+import java.util.List;
+
+import com.rayweb.proposedtalk.domain.Tag;
 import com.rayweb.proposedtalk.domain.Talk;
+import com.rayweb.proposedtalk.service.TagService;
 import com.rayweb.proposedtalk.service.TalkService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,21 +19,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/talks")
 public class TalkController {
 
-    private final TalkService talkservice;
+    private final TalkService talkService;
+    private final TagService tagService;
 
-    public TalkController(TalkService talkService){
-        this.talkservice = talkService;
+    public TalkController(TalkService talkService,TagService tagService){
+        this.talkService = talkService;
+        this.tagService = tagService;
     }
 
     @GetMapping(value = "/add")
-    public String addTalk(){
+    public String addTalk(Model model){
+        List<Tag> tags = tagService.findAll();
+        model.addAttribute("tags",tags);
         return "addTalk";
     }
 
 
     @PostMapping(value = "/add")
-    public ResponseEntity<Talk> addTalk(@RequestBody Talk request){
-        Talk talk = talkservice.create(request);
+    public ResponseEntity<Talk> addTalk(@RequestBody Talk proposedTalk){
+        Talk talk = talkService.create(proposedTalk);
         return ResponseEntity.ok(talk);
     }
 
--- src/main/resources/static/scripts/talks.js
+++ src/main/resources/static/scripts/talks.js
@@ -1,33 +1,72 @@
 
 
 function setMessage(text, type) {
-    $('.alert').show();
+	$('.alert').show();
 	$(".alert").text(text).css('font-weight', 'bold');
 	$('.alert').removeClass(
-			'alert-primary alert-danger alert-success alert-warning');
+		'alert-primary alert-danger alert-success alert-warning');
 	$('.alert').addClass('alert-' + type);
 	$(".alert").alert();
 }
 
-function addButton(tag){
-    var topic = $('<button type="button" class="btn btn-success">' + tag.name + '</button>');
-    $("#topics").append(topic);
-    $("#topic").val("");
+function addButton(tag) {
+	var topic = $('<button type="button" class="btn btn-success">' + tag.name + '</button>');
+	$("#topics").append(topic);
+	$("#topic").val("");
 }
-
-
-$(document).on("click", "#submit-topic", function(e) {
-    e.stopPropagation();
+function getTalk(){
+	var title = $("#title").val();
+	var description = $("#description").val();
+	var topics = []; 
+	$("#selectedTopics .btn-success").each (function() {
+		var tag = {
+			id : this.id,
+			name : this.value
+		}
+		topics.push(tag);
+	});
+	var proposedTalk = {
+		title : title,
+		description : description,
+		tags : topics
+	}
+	return proposedTalk;
+}
+$(document).on("click", "#selectedTopics .btn-primary, .btn-success", function (e) {
+	$(e.target).toggleClass("btn-primary btn-success");
+});
+$(document).on("click", "#submit-topic", function (e) {
+	e.stopPropagation();
 	var topic = $("#topic").val();
 	var url = "/tags/add?topic=" + topic;
 	$.ajax({
-		url : url,
-		type : 'POST',
-		error : function(request, status, error) {
+		url: url,
+		type: 'POST',
+		error: function (request, status, error) {
 			setMessage(request.responseJSON.message, "danger");
 		},
-		success : function(data) {
+		success: function (data) {
 			addButton(data);
+		}
+	});
+});
+
+$(document).on("click", "#summit-talk", function (e) {
+	e.stopPropagation();
+	var proposedTalk = getTalk();
+	var url = "/talks/add"
+	$.ajax({
+		url: url,
+		contentType : 'application/json; charset=utf-8',
+		dataType : 'json',
+		data: JSON.stringify(proposedTalk),
+		type: 'POST',
+		error: function (request, status, error) {
+			setMessage(request.responseJSON.message, "danger");
+		},
+		success: function (data) {
+			debbuger;
+			console.log(data);
 		}
 	});
 });
